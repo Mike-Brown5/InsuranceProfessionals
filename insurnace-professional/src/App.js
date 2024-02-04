25d781bcd03c30f1ced36a731a70b95aeb5233a1
mike-brown5 <mikeaxe5@outlook.com>
2024-02-04 15:54:16 +0200

    adding the templates and the components

---
+++ insurnace-professional/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <nav>
+    <a href="./">Home      </a>
+    <a href="./">Market Listings          </a>
+    <a href="./">Login      </a>
+    <a href="./">Join</a>
+    </nav>
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
