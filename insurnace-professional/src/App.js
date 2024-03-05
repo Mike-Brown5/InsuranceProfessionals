4edb360a0e11dd47eb191b9b3ebe0f21c4cf4b22
mike-brown5 <mikeaxe5@outlook.com>
2024-03-05 14:26:11 +0200

    register.js added

--- insurnace-professional/src/App.js
+++ insurnace-professional/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 
+import Register from './Register';
+
 function App() {
   return (
-    <div className="App">
-    <nav>
-    <a href="./">Home      </a>
-    <a href="./">Market Listings          </a>
-    <a href="./">Login      </a>
-    <a href="./">Join</a>
-    </nav>
-      <header className="App-header">
-
-      </header>
-    </div>
+    <main className="App">
+      <Register/>
+    </main>
   );
 }
 
